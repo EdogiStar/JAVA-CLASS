e3d1e13b4c80757edd8a58cc0dc60c87d36dab6c
EdogiStar <alhajimuhammadisah@gmail.com>
2021-09-17 11:15:43 +0100

    Add files via upload

---
+++ Assignment.java
@@ -0,0 +1,22 @@
+class Gr.avityCalc{}
+    Public Static void main(string[]args){
+
+    double gravity=-9.81;
+    double initialVelocity=0.0;
+    double fallingTime=10.0;
+    double initaialPosition=0.0;
+    double finalPosition=0.5;
+    gravity(fallingTime*fallingTime)+initialVelocity+initaialPosition;
+        system.Out.Println("the object's Position After"+falling Time+"seconds is"+finalPosition+"m");
+        }
+        }
+
+
+
+
+
+
+
+
+
+    9
